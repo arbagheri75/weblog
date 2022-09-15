8079feea00034d226f99e3c997c4ac782125934c
arbagheri75 <ar.bagheri0075@gmail.com>
2022-09-15 21:31:53 +0430

    add view to blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Post
+from .forms import PostModelForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'post_list'
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_list.html'
+
+
+class PostCreateView(generic.CreateView):
+    form_class = PostModelForm
+    template_name = 'blog/post_create.html'
+
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    form_class = PostModelForm
+    template_name = 'blog/post_create.html'
+
+
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('post_list')
