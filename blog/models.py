908204dfe972d98338853f318b6e2bf7a1d30d73
arbagheri75 <ar.bagheri0075@gmail.com>
2022-09-15 21:18:07 +0430

    create blog app and built models and admin

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+class Post(models.Model):
+    STATUS_CHOICE = (
+        ('pub', 'publish'),
+        ('drf', 'draft'),
+    )
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=3, choices=)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_list', args=[self.id])
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # Local App
     'accounts',
+    'blog',
 ]
 
 MIDDLEWARE = [
